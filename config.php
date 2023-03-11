46482c46674712e00768af7bcd91488539492599
Lorxiic <48385637+Lorxiic@users.noreply.github.com>
2023-03-11 09:49:52 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+$host_db = "heroku_a74d99c0cf8788f";
+$host_user = "b62ccc370d47b5";
+$host_pass = "5afeecdf";
+$host_aja = "us-cdbr-east-06.cleardb.net";
+
+$connect = new mysqli(host_db,host_user,host_pass,host_aja);
+?>
