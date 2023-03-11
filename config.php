056822d15642918ccae2b817d789c56ac0865ef6
Lorxiic <48385637+Lorxiic@users.noreply.github.com>
2023-03-11 10:19:09 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php 
-$host_db = "heroku_a74d99c0cf8788f";
+$host_db = "us-cdbr-east-06.cleardb.net";
 $host_user = "b62ccc370d47b5";
 $host_pass = "5afeecdf";
-$host_aja = "us-cdbr-east-06.cleardb.net";
+$host_aja = "heroku_a74d99c0cf8788f";
 
-$connect = new mysqli(host_db,host_user,host_pass,host_aja);
-?>
+
+$connect = mysqli_connect($host_db,$host_user,$host_pass,$host_aja) or die (" kontol");
+
