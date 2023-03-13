ebb6370ba88dedaf875e5788fa933a01fa470bab
Lorxiic <48385637+Lorxiic@users.noreply.github.com>
2023-03-13 08:51:26 +0000

    Update Asu.php

--- Asu.php
+++ Asu.php
@@ -38,50 +38,64 @@ echo "Menerima input ";
 
 //Perulangan tak terhingga untuk menerima dan memproses input
 while(true){
-	echo "*";
+ echo "*";
     $sys_jam = date("H");
     $sys_menit = date("i");
     $sys_detik = date("s");
     $sys_tanggal = date("d");
-	$data_input = dapatkanUpdate($offset);
-	if(!empty($data_input)){
+ $data_input = dapatkanUpdate($offset);
+ if(!empty($data_input)){
     foreach($data_input as $input){
-		//buat variabel data input
-		$id_chat = $input["message"]["chat"]["id"];
-		$id_user = $input["message"]["from"]["id"];
-		$in_teks = $input["message"]["text"];
-		$nm_user = $input["message"]["from"]["first_name"];
-		//proses input
-			//pisahkan teks input menjadi 2 bagian
-			$p2_teks = explode(' ',$in_teks,2);
-			//deteksi perintah
-			switch($p2_teks[0]){
-				case "/start": case "start$username_bot":
-					$ot_teks = "Hai ".$nm_user;
-				break;
-				case '/id': case "/id$username_bot" : 
-					$ot_teks = "$nm_user, ID kamu adalah $id_user";
-				break; 
-				case '/waktu': case "/waktu$username_bot" :
-					$ot_teks = "$nm_user, waktu server sekarang adalah :\n";
-					$ot_teks .= date("d M Y")."\nPukul ".date("H:i:s");
-				break;         
+  //buat variabel data input
+  $id_chat = $input["message"]["chat"]["id"];
+  $id_user = $input["message"]["from"]["id"];
+  $in_teks = $input["message"]["text"];
+  $nm_user = $input["message"]["from"]["first_name"];
+  //proses input
+   //pisahkan teks input menjadi 2 bagian
+   $p2_teks = explode(' ',$in_teks,2);
+   //deteksi perintah
+   switch($p2_teks[0]){
+    case "/start": case "start$username_bot":
+     $ot_teks = "Hai ".$nm_user;
+    break;
+    case '/id': case "/id$username_bot" : 
+     $ot_teks = "$nm_user, ID kamu adalah $id_user";
+    break; 
+    case '/waktu': case "/waktu$username_bot" :
+     $ot_teks = "$nm_user, waktu server sekarang adalah :\n";
+     $ot_teks .= date("d M Y")."\nPukul ".date("H:i:s");
+    break;
+                case "/encode": case "/encode$username_bot" :
+                  $base64 = base64_encode($p2_teks[1]);
+                  $eval   = "eval(base64_decode(".$base64."))";
+                  $utf8   = utf8_decode($eval);
+                  $ot_teks =   $utf8;
+                break;
+                case "/hitung": case "/hitung$username_bot" :
+                    //Operasi Penghitungan
+                    $i_hitung_1 = trim(str_ireplace("X","x",str_ireplace("*","x",str_ireplace("×","x",str_ireplace("÷","/", $p2_teks[1])))));
+                    $i_hitung_2 = $i_hitung_1;
+                    $operator = trim(str_ireplace('1',"",str_ireplace('2',"",str_ireplace('3',"",str_ireplace('4',"",str_ireplace('5',"",str_ireplace('6',"",str_ireplace('7',"",str_ireplace('8',"",str_ireplace('9',"",str_ireplace('0',"", $i_hitung_1)))))))))));
+                    $variabel_hitung = trim(str_ireplace("+","|",str_ireplace("-","|",str_ireplace("x","|",str_ireplace("/","|",str_ireplace("%","|", $i_hitung_2))))));
+                    include('hitung.php');
+                break;
                 case "/test":
-                    kirimPerintah("sendMessage",array("chat_id" => $id_chat, "text" => "Nyaaaa!!"));
+		   kirimPerintah("sendMessage",array("chat_id" => $id_chat, "text" => "Nyaaaa!!"));
                     $ot_teks = "huh.";
                 break;
-        				default:
+            default:
                             $hasil = prosesPesanTeks($in_teks, "data_pesan_teks.txt");
-        					if(!empty($hasil)) $ot_teks = stringVariabel($hasil);
+             if(!empty($hasil)) $ot_teks = stringVariabel($hasil);
                             else $ot_teks = $in_teks;
-        				break;
-			}
-		//kirimkan output
-		if(isset($ot_teks))     { kirimPerintah("sendMessage",array("chat_id" => $id_chat, "text" => $ot_teks, "parse_mode" => "Markdown")); echo "\nuser mengatakan : ".$in_teks." kepada bot\nbot membalas = ".$ot_teks."\n"; unset($ot_teks); }
+            break;
+   }
+  //kirimkan output
+  if(isset($ot_teks))     { kirimPerintah("sendMessage",array("chat_id" => $id_chat, "text" => $ot_teks, "parse_mode" => "Markdown")); echo "\nuser mengatakan : ".$in_teks." kepada bot\nbot membalas = ".$ot_teks."\n"; unset($ot_teks); }
         if(isset($ot_teks_2))   { kirimPerintah("sendMessage",array("chat_id" => $id_chat, "text" => $ot_teks_2)); echo "\nbot membalas = ".$ot_teks." kepada user\n"; unset($ot_teks_2); }
-		//tambahkan offset agar proses tidak berulang
-		$offset = $input["update_id"] + 1;
-	}
+  //tambahkan offset agar proses tidak berulang
+  $offset = $input["update_id"] + 1;
+ }
     echo "\nMenerima input ";
     }
     sleep(1);
